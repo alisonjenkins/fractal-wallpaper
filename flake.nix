6881f9cb1e6466c0044b0af46a39c4735a400552
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2026-03-28 21:56:52 +0000

    feat: add flake with devshell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rust
+            pkgs.cargo-watch
+          ];
+        };
+      }
+    );
+}
